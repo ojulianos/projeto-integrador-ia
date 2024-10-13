c5c9b0d250887ec0836fb8bd0e48d144bb7db292
Paulo Cesar Dal Ponte <paulocesardalponte@gmail.com>
2024-10-13 17:51:50 -0300

    api com gpt

--- back-end/app/views/chat.py
+++ back-end/app/views/chat.py
@@ -2,7 +2,7 @@ from werkzeug.security import generate_password_hash
 from app import db
 from flask import request, jsonify
 from ..models.history import History, History_schema, Historys_schema
-from openai import OpenAI
+import openai
 
 """Retorna detalhes do chatGPT e histórico"""
 def get_chats(user_id = ""):
@@ -27,37 +27,44 @@ def get_chat(id):
     return jsonify({'message': "history don't exist", 'data': {}}), 404
 
 def get_gpt():
-    prompt = ""
-    profissao = request.json['profissao']
 
+    cargo = request.json.get('cargo', '')
+    tecnologia = request.json.get('tecnologia', '')
+    tempoMaximo = request.json.get('tempoMaximo', '')
+    tempoMedio = request.json.get('tempoMedio', '')
+    formatoEstudos = request.json.get('formatoEstudos', '')
 
-    prompt = "ola chat gepeto, preciso de detalhes para chegar a profissao " + profissao
+    prompt = f"""
+    Eu preciso saber quais as etapas para chegar ao cargo de {cargo}, na tecnologia {tecnologia},gostaria que me adequasse para que leve no máximo de {tempoMaximo},
+    sendo que possuo um tempo médio semanal de {tempoMedio}, preciso que me indique principalmente o formato de didádica {formatoEstudos}.
+    """
 
-    return jsonify(prompt)
+    openai.api_key=""
 
-    client = OpenAI()
-
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo-0125",
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
         messages=[
             {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "textoGPT"
-                    }
-                ]
-            }
-        ],
-        temperature=1,
-        max_tokens=2048,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        response_format={
-            "type": "json"
+            "role": "system",
+            "content": """
+                Você é um assistente que vai dar respostas diretas, relacionadas a o que a pessoa deve estudar para alcançar seus obejtivos.
+                Preciso que você sugira livros, sites de acordo com a didatica escolhida pelo usuário, e o tempo total que o usuário deseja, 
+                será informado no prompt, mas você ficará com a responsabilidade de separar o tempo de cada etapa para se tornar apto.
+                O usuário também terá a possibilidade de importar um pdf, onde será lhe passado no prompt, as informações contidas, preciso que analise 
+                o currículo e capte as experiências que o usuário já possui, tornando sua resposta mais acertiva, busque sempre procurar opções de indicação gratuitas
+                Sempre responda ao usuário no seguinte formato de Json. 
+            """
+        },
+            {
+            "role" : "user",
+            "content" : prompt
         }
+        ],
+        temperature=0.5,
+        max_tokens=2048,
+        top_p=0.9,
+        frequency_penalty=1,
+        presence_penalty=1,
     )
-    return response
+    return response.choices[0].message.content
 
